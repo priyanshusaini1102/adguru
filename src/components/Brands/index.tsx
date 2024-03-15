64740c915e31f6ba634f2b9c5a655b5589e62cfa
priyanshusaini1102 <priyanshusaini1102@gmail.com>
2024-03-15 10:59:58 +0530

    fix brands

--- src/components/Brands/index.tsx
+++ src/components/Brands/index.tsx
@@ -31,12 +31,13 @@ const SingleBrand = ({ brand }: { brand: Brand }) => {
   const { href, image, imageLight, name, id } = brand;
 
   return (
-    <div className="flex w-1/2 items-center justify-center px-3 py-[15px] sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6">
+    <div className="">
       <span
-        className="relative h-10 w-full opacity-70 transition hover:opacity-100 dark:opacity-60 dark:hover:opacity-100"
+        className="w-full transition"
       >
-        <Image src={imageLight} alt={name} fill className="hidden dark:block" />
-        <Image src={image} alt={name} fill className="block dark:hidden" />
+        <div className={`h-16 w-16 md:h-20 md:w-20 relative rounded-full overflow-hidden cover p-4 `}>
+          <Image src={imageLight} alt={name} fill />
+        </div>
       </span>
     </div>
   );
