1a73b2cf29886489078c147c06f3bef0227dcd3e
priyanshusaini1102 <priyanshusaini1102@gmail.com>
2024-04-05 23:40:35 +0530

    changes

--- src/app/schedule-call/page.tsx
+++ src/app/schedule-call/page.tsx
@@ -18,7 +18,7 @@ const Page = () => {
   <div className="spinner border-4 border-t-4 border-gray-200 rounded-full animate-spin h-16 w-16"></div>
 </div>}
       <iframe
-        src="https://calendly.com/priyanshusaini1102/30min?hide_gdpr_banner=1"
+        src="https://calendly.com/adgurunow/30min?hide_gdpr_banner=1"
         frameBorder="0"
         width="100%"
         height="100%"
