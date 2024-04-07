5aba7b237f19369c5a3e4aa536180580ea69e89a
priyanshusaini1102 <priyanshusaini1102@gmail.com>
2024-04-07 15:24:20 +0530

    add insta link

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,8 +5,30 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <>
-      <footer className="m-4 rounded-lg bg-white shadow dark:bg-gray-800">
-        <div className="mx-auto w-full max-w-screen-xl p-4 text-center">
+      <footer className="m-4 px-40 w-full rounded-lg bg-white shadow dark:bg-gray-800 flex justify-between items-center content-center">
+        <div className="flex items-center justify-center gap-4">
+          <span className="text-sm text-gray-500 dark:text-gray-400 sm:text-center">
+          Follow: 
+          </span>
+        <div className="">
+          <button className="bg-white text-lightBlue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
+            <a href="https://www.instagram.com/adguruofficial" target="_blank" className="flex justify-center items-center">
+            <Image width={20} height={20} src="/images/logo/instagram.png" alt="" />
+            </a>
+          </button>
+          <button className="bg-white text-lightBlue-600 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
+          <div className="flex justify-center items-center">
+            <Image width={20} height={20} src="/images/logo/linkedin.png" alt="" />
+            </div>
+          </button>
+          <button className="bg-white text-pink-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
+          <div className="flex justify-center items-center">
+            <Image width={20} height={20} src="/images/logo/whatsapp.png" alt="" />
+            </div>
+          </button>
+        </div>
+        </div>
+        <div className=" p-4 text-right">
           <span className="text-sm text-gray-500 dark:text-gray-400 sm:text-center">
             © 2024{" "}
             <span>
