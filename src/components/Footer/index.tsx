1ca9f8b05aaeac4154345b52c8c2fc8fb0f2714a
priyanshusaini1102 <priyanshusaini1102@gmail.com>
2024-04-07 15:25:49 +0530

    fix

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
           <span className="text-sm text-gray-500 dark:text-gray-400 sm:text-center">
           Follow: 
           </span>
-        <div className="">
+        <div className="flex items-center gap-1">
           <button className="bg-white text-lightBlue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
             <a href="https://www.instagram.com/adguruofficial" target="_blank" className="flex justify-center items-center">
             <Image width={20} height={20} src="/images/logo/instagram.png" alt="" />
